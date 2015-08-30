2a6d746606f368b48013d17d321116ff20a2e56c
EileenPunk <eileenpunk922@gmail.com>
2015-08-30 16:45:43 +0200

    aside structure added, home page is building up

--- src/assets/javascripts/components/scripts.js
+++ src/assets/javascripts/components/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
+    $(".toCategories").click(function() {
+        $('html, body').animate({
+            scrollTop: $('#Categories').offset().top
+        },1000);
+    });
 
+    $(".toContact").click(function() {
+        $('html, body').animate({
+            scrollTop: $('#Contact').offset().top
+        },1000);
+    });
+
+    $(".toFooter").click(function() {
+        $('html, body').animate({
+            scrollTop: $('#Footer').offset().top
+        },1000);
+    });
 });
