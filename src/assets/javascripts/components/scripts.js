6de35639098f7fecb8eb2770664af8a47c2d57a6
EileenPunk <eileenpunk922@gmail.com>
2015-09-06 21:59:59 +0200

    scroll down and to top, aside in all pages done

--- src/assets/javascripts/components/scripts.js
+++ src/assets/javascripts/components/scripts.js
@@ -1,19 +1,21 @@
 $(document).ready(function () {
+    var topbarHeight = $('.Header').height()+20;
+
     $(".toCategories").click(function() {
         $('html, body').animate({
-            scrollTop: $('#Categories').offset().top
+            scrollTop: $('#Categories').offset().top-= topbarHeight
         },1000);
     });
 
     $(".toContact").click(function() {
         $('html, body').animate({
-            scrollTop: $('#Contact').offset().top
+            scrollTop: $('#Contact').offset().top-= topbarHeight
         },1000);
     });
 
     $(".toFooter").click(function() {
         $('html, body').animate({
-            scrollTop: $('#Footer').offset().top
+            scrollTop: $('#Footer').offset().top-= topbarHeight
         },1000);
     });
 });
