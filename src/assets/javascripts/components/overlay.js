2fd64d81beb2ecf3c04ea6ab9bb8b0a12d4027ea
EileenPunk <eileenpunk922@gmail.com>
2015-09-14 20:33:56 +0200

    done and backuped

--- src/assets/javascripts/components/overlay.js
+++ src/assets/javascripts/components/overlay.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
     $(".mainSection").on("click", "a.js-preview", function(){
         var btnId = $(this).attr('id');
         $( "."+ btnId ).css('display','block');
+        $( "body" ).css('overflow','hidden');
+        $( ".cd-top" ).css('display','none');
+        $( ".FooterContent" ).css('display','none');
     });
 
     $.fn.overlayClose = function() {
         $( ".overlay" ).css('display','none');
+        $( "body" ).css('overflow','auto');
+        $( ".cd-top" ).css('display','block');
+        $( ".FooterContent" ).css('display','block');
         return this;
     };
 });
