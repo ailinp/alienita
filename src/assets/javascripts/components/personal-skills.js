b8641b7de11bcfc7f17a62c3ff04bc9cfee2fcd1
Peng <penga@LPA-H-MAC-06.lan>
2019-12-18 10:43:17 +0000

    website update

--- src/assets/javascripts/components/personal-skills.js
+++ src/assets/javascripts/components/personal-skills.js
@@ -1,14 +1,12 @@
 jQuery(document).ready(function($) {
-    $(window).scroll(function() {
-        $('.PersonalSkills-chart').easyPieChart({
-            scaleColor: false,
-            lineWidth: 5,
-            lineCap: 'round',
-            barColor: '#f64159',
-            trackColor: '#fcbbc3',
-            size: 120,
-            animate: 5000,
-            onStop: $.noop
-        });
+    $('.PersonalSkills-chart').easyPieChart({
+        scaleColor: false,
+        lineWidth: 5,
+        lineCap: 'round',
+        barColor: '#f64159',
+        trackColor: '#fcbbc3',
+        size: 120,
+        animate: 5000,
+        onStop: $.noop
     });
 });
